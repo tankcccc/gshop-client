bed8a1882ba40209b9beaf634ca228a16fe8c229
tankccc <540153461@qq.com>
2019-10-23 11:39:56 +0800

    提交

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <div>
+    	<router-view/> <!--是一个占位符，用来装当前的组件。-->
+    	<FooterGuide/>
+    </div>
+  </div>
+</template>
+
+<script>
+import FooterGuide from './components/FooterGuide/FooterGuide.vue'
+
+export default {
+  name: 'app',
+  components: {
+    FooterGuide
+  }
+}
+</script>
+
+<style lang ="stylus" rel="stylesheet/stylus">
+#app
+	color:red
+</style>
---
+++ src/components/FooterGuide/FooterGuide.vue
@@ -0,0 +1,77 @@
+<template>
+	<div class="footer_guide">
+    <span class="guide_item" :class="{on: '/msite'===$route.path}" @click="goTo('/msite')"><!--当前的路径等于请求路径-->
+      <span class="item_icon">
+        <i class="iconfont icon-sousuo"></i>
+      </span>
+      <span>外卖</span>
+    </span>
+    <a href="javascript:;" class="guide_item" :class="{on: '/search'===$route.path}" @click="goTo('/search')">
+      <span class="item_icon">
+        <i class="icon-weibiaoti-"></i>
+      </span>
+      <span>搜索</span>
+    </a>
+	<!--前面路径class代表你输入路径，跳转到哪里，前面是类名后边是布尔值，，，，，，，，，，当前的路径如果等于请求的路径-->
+    <a href="javascript:;" class="guide_item" :class="{on: '/order'===$route.path}" @click="goTo('/order')">
+      <span class="item_icon">
+        <i class="iconfont icon-dingdan"></i>
+      </span>
+      <span>订单</span>
+    </a>
+    <a href="javascript:;" class="guide_item" :class="{on: '/profile'===$route.path}" @click="goTo('/profile')">
+      <span class="item_icon">
+        <i class="iconfont icon-geren"></i>
+      </span>
+      <span>我的</span>
+    </a>
+  </div>
+</template>
+
+<script>
+	// export default {
+	// 	methods:{
+	// 		goTo(path) {
+	// 			this.$route.replace(path)
+	// 		}
+	// 	}
+	// }
+	 export default {
+	    methods:{
+	      goTo (path) {
+	        this.$router.replace(path)
+	      }
+	    }
+	  }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+  @import "../../common/stylus/mixins.styl"
+  .footer_guide  //footer
+    top-border-1px(#e4e4e4)
+    position fixed
+    z-index 100
+    left 0
+    right 0
+    bottom 0
+    background-color #fff
+    width 100%
+    height 50px
+    display flex
+    .guide_item
+      display flex
+      flex 1
+      text-align center
+      flex-direction column
+      align-items center
+      margin 5px
+      color #999999
+      &.on
+        color #02a774
+      span
+        font-size 12px
+        margin-top 2px
+        margin-bottom 2px
+        .iconfont
+          font-size 22px
+</style>
